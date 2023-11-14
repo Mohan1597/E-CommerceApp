927a0649be23df8cb258ac0a64c9fcc5efb1e515
Mohan <mohan.nimmala@bluehublabs.net>
2023-11-14 22:19:32 +0530

    TopBar(HomePage)

--- ecommerce-app/src/Components/Registartion/HomePage.jsx
+++ ecommerce-app/src/Components/Registartion/HomePage.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import ResponsiveAppBar from '../MUIComponents/HomeBar.tsx'
+import { Grid } from '@mui/material'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <Grid container spacing={0}>
+        <Grid item xs={12}>
+           <ResponsiveAppBar />
+        </Grid>
+        <Grid item xs={12}>
+           <img src='Images\HomePage\home-bg.AVIF' alt='' style={{width: '100%'}}/>
+        </Grid>
+        
+    </Grid>
   )
 }
 
