184fa58d1ba25765e66bbfd1064933f206527abb
Mohan <mohan.nimmala@bluehublabs.net>
2024-04-14 20:02:19 +0530

    Api First Commit

--- ecommerce-app/API/ECommerceAPI/obj/Debug/net8.0/ECommerceAPI.AssemblyInfo.cs
+++ ecommerce-app/API/ECommerceAPI/obj/Debug/net8.0/ECommerceAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebb0897374d10eb19fa4c86b7d19e504fc14556d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0956298b605de098b63f0085ca6ed1c04f0775bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
