7c377180ef5277186196994164388aa5ff93e501
Mohan <mohan.nimmala@bluehublabs.net>
2024-04-14 20:22:10 +0530

    #API Commit 2

---
+++ ecommerce-app/API/ECommerceAPI/Controllers/RegistartionController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceAPI.Controllers
+{
+    public class RegistartionController : Controller
+    {
+        [HttpGet("/login")]
+        public string Login()
+        {
+            return "Hello Worlds";
+        }
+    }
+}
---
+++ ecommerce-app/API/ECommerceAPI/Models/LoginModal.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerceAPI.Models
+{
+    public class LoginModal
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+    }
+}
