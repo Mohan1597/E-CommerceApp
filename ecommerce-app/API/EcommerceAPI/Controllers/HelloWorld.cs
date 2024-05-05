0cd29b79a49a69b395779332eefa684a276f98b3
Mohan <mohan.nimmala@bluehublabs.net>
2024-05-05 23:34:19 +0530

    Added Folders

---
+++ ecommerce-app/API/EcommerceAPI/Controllers/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceAPI.Controllers
+{
+    public class HelloWorld : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("/v1/HelloWorld")]
+        public string HelloWorldP()
+        {
+            return "Hello World";
+        }
+    }
+}
