c1eb338bd7014170203f8607a01cb66951bb08c9
Mohan <mohan.nimmala@bluehublabs.net>
2024-05-05 15:05:08 +0530

    First Commit API

--- ecommerce-app/API/EcommerceAPI/Controllers/WeatherForecastController.cs
+++ ecommerce-app/API/EcommerceAPI/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace EcommerceAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing","Mohan" ,"Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
